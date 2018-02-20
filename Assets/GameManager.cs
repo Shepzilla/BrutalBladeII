cbcff07c2f9ee7970b39040f92f0166529aa852e
Robert J Harper <flamesofpower15@hotmail.com>
2018-02-20 13:09:26 +0000

    Managers and Blood
    **FEATURES**
    +Blood spurt particle system.
    +The basis of game managers to handle the transitions between game states.
    +The basis of the kill cam manager which may or may not be used for dramatic camera cuts during gameplay.
    
    **BUGS/FLAWS**
    Since nothing has actually been put in scene yet, there are no bugs to detect.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public int roundLimit = 3;              //The amount of round wins required to win the game.
+    public float timeToStart = 3.0f;        //The delay before a round starts.
+    public float timeToFinish = 5.0f;       //The delay before the level is reset.
+
+    private WaitForSeconds startDelay;
+    private WaitForSeconds endDelay;
+    private int currentRound = 0;           //The round the game is currently on.
+
+	// Use this for initialization
+	void Start () {
+        //Sets up time delays according to values assigned in the inspector.
+        startDelay = new WaitForSeconds(timeToStart);
+        endDelay = new WaitForSeconds(timeToFinish);
+
+        //Executes main coroutine.
+	}
+	
+    //This handles the calling of the different phases of a round.
+	private IEnumerator GameLoop()
+    {
+        //This will not return until the start of a round has completed.
+        yield return StartCoroutine(RoundStart());
+
+        //This will not return until the round is finished.
+        //yield return StartCoroutine(RoundPlay());
+
+        //This will not return until the end of a round has completed.
+        //yield return StartCoroutine(RoundEnd());
+
+        //Checks to see if an overall winner exists, if so. Reset the game.
+        /* if (GameWinner)
+         * {
+         *      Application.LoadLevel(Application.loadedLevel);
+         * }
+        */
+        //Otherwise, restart the coroutine (begin another round).
+        StartCoroutine(GameLoop());
+    }
+
+    private IEnumerator RoundStart()
+    {
+        currentRound++;
+        yield return startDelay;
+    }
+}
---
+++ Assets/KillCamManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// This moves the actor to the centerpoint between two or more players.
+/// The purpose of this actor is to carry positions for potential kill cameras to cut to.
+/// </summary>
+public class KillCamManager : MonoBehaviour {
+
+    GameObject[] players;           //Array of players present in the level.
+    Vector3 midPoint;               //The calculated point in between all the players.
+	
+    // Use this for initialization
+    // Gets a reference to all the players spawned in the level.
+	void Start () {
+        if (players == null)
+        {
+            players = GameObject.FindGameObjectsWithTag("Player");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Gathers the position of all the players in the level.
+		foreach (GameObject player in players)
+        {
+            midPoint +=  player.transform.position;
+        }
+        //Divides by the length of the player array to calculate the average position.
+        midPoint /= players.Length;
+        transform.position = midPoint;                              //Set the gameObject transform to the calculated average.
+        transform.rotation = players[0].transform.rotation;         //Set the gameObject rotation to match that of player 1.
+	}
+}
