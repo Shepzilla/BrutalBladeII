ce73f35cdb873f365547c503babf4b0477f41ddf
Robert J Harper <flamesofpower15@hotmail.com>
2018-03-13 12:53:52 +0000

    Post Process and Animation Update
    **FEATURES**
    + The player's chest now gets involved in attacks, increasing attack range.
    + Adaptive depth of field.
    + Split Screen

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PostProcessing;
 
 public class FollowCamera : MonoBehaviour {
 
@@ -10,9 +11,18 @@ public class FollowCamera : MonoBehaviour {
     private Vector3 velocity = Vector3.zero;
     Transform enemyTarget;
 
+    PostProcessingProfile ppProfile;
+
+    
+
     void Start()
     {
         enemyTarget = target.opponent;
+        ppProfile = GetComponent<PostProcessingBehaviour>().profile;
+        if (ppProfile != null)
+        {
+            print("gateem");
+        }
     }
 
     void Update()
@@ -21,5 +31,16 @@ public class FollowCamera : MonoBehaviour {
         transform.LookAt(new Vector3 (enemyTarget.position.x, enemyTarget.position.y + 0.5F, enemyTarget.position.z));
         //transform.rotation.SetLookRotation(enemyTarget.position);
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        AdaptiveDepthOfField();
+    }
+
+    void AdaptiveDepthOfField()
+    {
+        //Gets the current depth of field settings and then gets the distance between the camera and the player's enemy.
+        DepthOfFieldModel.Settings dofFocus = ppProfile.depthOfField.settings;
+        dofFocus.focusDistance = ((transform.position - enemyTarget.position).magnitude);
+
+        //Sets focus distance to distance calculated.
+        ppProfile.depthOfField.settings = dofFocus;
     }
 }
