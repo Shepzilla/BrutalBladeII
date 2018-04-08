ec263911a29c20012735546c8296a1288c921679
Shepzilla <flamesofpower15@hotmail.com>
2018-04-08 01:53:01 +0100

    Failed Damping Fix
    Features SmoothDamp but did not get desired results.
    
    Will move to torque usage on next pass.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,7 +15,8 @@ public class PlayerController : MonoBehaviour
     //Public Variables.
     public int playerNumber = 1;                //Stores player number for correct input.
     public float movementDamping = 0.1f;        //Smooths stick input.
-    public float swordDamping = 0.2f;           //Smooths sword movement.
+    public float swingSpeed = 5.0f;             //How fast can the player swing their sword?
+    public float legacySwordDamping = 0.2f;     //Smooths sword movement. (Legacy)
     public float swingExtreme = 0.4f;           //Angle limit for sword movement.
     public float modifierExtreme = 0.8f;        //Angle limit for grip modifier.
     public GameObject armParent;                 //Quick and easy way to move both arms and keep them in sync.
@@ -24,6 +25,7 @@ public class PlayerController : MonoBehaviour
     public Transform rightIkTarget = null;      //Where the right hand goes.
     public Transform leftIkTarget = null;       //Where the left hand goes.
     public int criticalMultiplier = 2;          //How much extra damage is taken when a critical limb is hit.
+    public bool legacyMovement = false;         //Toggles between using linear interpolation or smoothDamp.
 
     //Private Variables.
     private float horizontalSword = 0;
@@ -31,6 +33,8 @@ public class PlayerController : MonoBehaviour
     private float swordModifier = 0;
     private PlayerHealth playerHealth;
     private Quaternion targetRotation;
+    private float xVelocity = 0.0f;
+    private float yVelocity = 0.0f;
 
     Animator animator;
     Rigidbody rigidBody;
@@ -52,9 +56,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        MoveCharacterAndSword();
+        if(legacyMovement)
+        {
+            LegacyMoveCharacterAndSword();
+        }
+        else
+        {
+            MoveCharacterAndSword();
+        }
     }
 
+    /// <summary>
+    /// This moves the player's sword around according to input from both triggers and the right analogue stick.
+    /// </summary>
     void MoveCharacterAndSword()
     {
         //Reads input device for input
@@ -67,9 +81,49 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("LeftRight", horizontal, movementDamping, Time.deltaTime);
 
         //Sword movement (with interpolation)
-        horizontalSword = Mathf.Lerp(horizontalSword, Input.GetAxis("HorizontalSword" + playerNumber), swordDamping * Time.deltaTime);
-        verticalSword = Mathf.Lerp(verticalSword, Input.GetAxis("VerticalSword" + playerNumber), swordDamping * Time.deltaTime);
-        swordModifier = Mathf.Lerp(swordModifier, Input.GetAxis("GripModifier" + playerNumber), swordDamping * Time.deltaTime);
+        horizontalSword = Mathf.SmoothDamp(horizontalSword, Input.GetAxis("HorizontalSword" + playerNumber), ref xVelocity, legacySwordDamping * Time.deltaTime, swingSpeed, Time.deltaTime);
+        verticalSword = Mathf.SmoothDamp(verticalSword, Input.GetAxis("VerticalSword" + playerNumber), ref yVelocity, legacySwordDamping * Time.deltaTime, swingSpeed, Time.deltaTime);
+        swordModifier = Mathf.Lerp(swordModifier, Input.GetAxis("GripModifier" + playerNumber), legacySwordDamping * Time.deltaTime);
+
+        //Clamps values of sword rotations to the specified extreme.
+        horizontalSword = Mathf.Clamp(horizontalSword, -swingExtreme, swingExtreme);
+        verticalSword = Mathf.Clamp(verticalSword, -swingExtreme, swingExtreme);
+        swordModifier = Mathf.Clamp(swordModifier, -modifierExtreme, modifierExtreme);
+
+        //Allows the editor game to be stopped (since the mouse in bound to the game screen.)
+        if (Input.GetKey("escape"))
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+
+        //Applies input values to armParent to move the sword around the screen.
+        targetRotation = new Quaternion(verticalSword, horizontalSword, swordModifier, 1);//transform.rotation.w);
+        armParent.transform.localRotation = targetRotation;
+
+        //Gets the opponent's position and sets the y to zero to prevent undersired vertical rotations.
+        opponentCoord = new Vector3(opponent.transform.position.x, 0, opponent.transform.position.z);
+        rigidBody.transform.LookAt(opponentCoord);
+    }
+
+
+    /// <summary>
+    /// This is the legacy version of sword control which utilises linear interpolation.
+    /// </summary>
+    void LegacyMoveCharacterAndSword()
+    {
+        //Reads input device for input
+        //Movement
+        float horizontal = Input.GetAxis("Horizontal" + playerNumber);
+        float vertical = Input.GetAxis("Vertical" + playerNumber);
+
+        //Applies input values to animator controller variables.
+        animator.SetFloat("ForwardBackward", vertical, movementDamping, Time.deltaTime);
+        animator.SetFloat("LeftRight", horizontal, movementDamping, Time.deltaTime);
+
+        //Sword movement (with interpolation)
+        horizontalSword = Mathf.Lerp(horizontalSword, Input.GetAxis("HorizontalSword" + playerNumber), legacySwordDamping * Time.deltaTime);
+        verticalSword = Mathf.Lerp(verticalSword, Input.GetAxis("VerticalSword" + playerNumber), legacySwordDamping * Time.deltaTime);
+        swordModifier = Mathf.Lerp(swordModifier, Input.GetAxis("GripModifier" + playerNumber), legacySwordDamping * Time.deltaTime);
 
         //Clamps values of sword rotations to the specified extreme.
         horizontalSword = Mathf.Clamp(horizontalSword, -swingExtreme, swingExtreme);
